4293ba244c37a2b9d5e3c1560ef40bb8e916b7d3
khouloudde <115475301+khouloudde@users.noreply.github.com>
2022-10-10 16:22:03 +0100

    Finds median for any list

--- median.py
+++ median.py
@@ -1,10 +1,23 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def findMedian(list):
+    list.sort()
+    length = len(list)
+
+    if length%2 != 0:
+        median = list[length//2]
+    else:
+        median = (list[int(length/2)]+list[int((length/2))-1])/2
+
+    print(f'Median value is --> {median}')
+
+
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+        findMedian(numbers)
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
